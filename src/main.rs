bd1d8bc9914d15371b879d2942f44cea8c40a6d7
veaba <908662421@qq.com>
2020-06-19 11:11:41 +0800

    update: nginx.conf format

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,11 @@
-extern crate regex;
+use std::fs::File;
+use std::io::Read;
+
+extern crate resolv_conf;
 fn main() {
-    let a = String::from("config=config.toml ad");
-    // let b = String::from("config=dsa=config.toml dasd "); //false
-    // let c = "configoml "; //false
-    // let a ="config=config.toml"; //true
-    let x = is_compose_cmd(&a);
-    println!("{:?}", x);
-    // let reg= regex::Regex::new(r"^.*=.*$").unwrap();
-    // let x =reg.is_match(b);
-    // println!("{}",x);
-
-    // for i in 0..3 {
-    //     println!("{}",i)
-    // };
-}
-
-fn is_compose_cmd(arg_string: &String) -> bool {
-    let mut vec_array = vec![];
-    let str_split = arg_string.split("=");
-    for code in str_split {
-        // vec_array.push(code);
-        vec_array.push(code.trim());
-    }
-    println!("array===>{:?}", vec_array);
-    if vec_array.len() != 2 {
-        return false;
-    }
-    return true;
+    let mut buf = Vec::with_capacity(4096);
+    let mut f = File::open("F:/Github/learn-rust/ngnix.conf").unwrap();
+    f.read_to_end(&mut buf).unwrap();
+    let cfg = resolv_conf::Config::parse(&buf).unwrap();
+    println!("---- Config -----\n{:#?}\n", cfg);
 }
